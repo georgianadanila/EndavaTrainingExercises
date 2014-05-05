9380a14610b4f8ce4d56d046fb4a3561129430b1
Georgiana Danila <georgiana.danila42@gmail.com>
2014-05-05 13:51:36 +0300

    Exceptions and Flow control exercises

---
+++ FlowControlAndExeptions/src/Main.java
@@ -0,0 +1,28 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		IfThenElse ifObj = new IfThenElse();
+		ifObj.print();
+		
+		System.out.println("----------");
+		
+		Switch switchObj = new Switch();
+		switchObj.switchExample();
+		
+		System.out.println("----------");
+		
+		Exceptions exceptObj = new Exceptions();
+		exceptObj.writeList();
+		
+		System.out.println("----------");
+		
+		FinallyExample finallyObj = new FinallyExample();
+		System.out.println("Returned value is: "+finallyObj.method());
+		System.out.println(finallyObj.method1());
+		finallyObj.method2();
+		
+	}
+
+}
