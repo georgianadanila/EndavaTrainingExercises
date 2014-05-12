45a3c46a523b7d66049f3af48d9196c935dbb1a8
Georgiana Danila <georgiana.danila42@gmail.com>
2014-05-12 13:04:36 +0300

    Improvements on RandomizedQueue

--- Collections/src/colections/homework/main/RandomizedQueue.java
+++ Collections/src/colections/homework/main/RandomizedQueue.java
@@ -1,6 +1,7 @@
 package colections.homework.main;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 import java.util.Random;
 
 import colections.homework.test.TestRandomizedQueue;
@@ -8,25 +9,24 @@ import colections.homework.test.TestRandomizedQueue;
 /**
  * 
  * @author Georgiana Danila
- *
+ * 
  */
 public class RandomizedQueue<Item> implements Iterable<Item> {
-	public Item[] randomizedQueue;
-	public int numberOfElements=0;
-	Random randomObject = new Random();
-	int randomIndex, lastIndex;
-	
+	private Item[] randomizedQueue;
+	private int numberOfElements;
+	private Random randomObject;
+	private int randomIndex, lastIndex;
+
 	// construct an empty randomized queue
 	public RandomizedQueue() {
-		randomizedQueue = (Item[])new Object[10];
+		randomizedQueue = (Item[]) new Object[10];
+		randomObject = new Random();
+		numberOfElements = 0;
 	}
 
 	// is the queue empty?
 	public boolean isEmpty() {
-		if (numberOfElements != 0) {
-			return false;
-		}
-		return true;
+		return (numberOfElements != 0);
 	}
 
 	// return the number of items on the queue
@@ -34,62 +34,87 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
 		return numberOfElements;
 	}
 
+	/*
+	 * in cazul in care se umple coada, se dubleaza dimensiunea ei
+	 */
+	public Item[] resizeOnEnqueue() {
+		Item[] temporaryQueue = (Item[]) new Object[randomizedQueue.length * 2];
+		for (int i = 0; i < randomizedQueue.length; i++) {
+			temporaryQueue[i] = randomizedQueue[i];
+		}
+		return temporaryQueue;
+	}
+	
 	// add the item
 	public void enqueue(Item item) {
+		if (item == null) {
+			throw new NullPointerException();
+		}
 		if (numberOfElements == randomizedQueue.length) {
-			Item[] temporaryQueue = (Item[])new Object[randomizedQueue.length*2];
-			for (int i=0; i<randomizedQueue.length; i++) {
-				temporaryQueue[i] = randomizedQueue[i];
-			}
-			randomizedQueue = temporaryQueue;
+			randomizedQueue = resizeOnEnqueue();
 		}
 		randomizedQueue[numberOfElements] = item;
 		numberOfElements++;
 	}
 
+	/*
+	 * in cazul in care numarul de elemente din coada ajunge la un sfert din dimensiunea ei
+	 * se face resize (dimensiunea se injumatateste)
+	 */
+	public Item[] resizeOnDequeue() {
+		Item[] temporaryQueue = (Item[]) new Object[randomizedQueue.length / 2];
+		for (int i = 0; i < numberOfElements; i++) {
+			temporaryQueue[i] = randomizedQueue[i];
+		}
+		return temporaryQueue;
+	}
+	
 	// delete and return a random item
 	public Item dequeue() {
+		if (numberOfElements == 0) {
+			throw new NoSuchElementException();
+		}
 		randomIndex = randomObject.nextInt(numberOfElements);
 		Item dequeuedItem = randomizedQueue[randomIndex];
-		if (numberOfElements == randomizedQueue.length/4) {
-			Item[] temporaryQueue = (Item[])new Object[randomizedQueue.length/2];
-			for (int i=0; i<numberOfElements; i++) {
-				temporaryQueue[i] = randomizedQueue[i];
-			}
-			randomizedQueue = temporaryQueue;
+		if (numberOfElements == randomizedQueue.length / 4) {
+			randomizedQueue = resizeOnDequeue();
 		}
-		//if (randomIndex != numberOfElements)
-			randomizedQueue[randomIndex] = randomizedQueue[numberOfElements-1];
+		// nu stergi elementul random din vector =>
+		// ultimul element trebuie sa fie ...
+		randomizedQueue[randomIndex] = randomizedQueue[numberOfElements - 1];
 		numberOfElements--;
 		return dequeuedItem;
 	}
 
 	// return (but do not delete) a random item
 	public Item sample() {
+		if (numberOfElements == 0) {
+			throw new NoSuchElementException();
+		}
 		randomIndex = randomObject.nextInt(numberOfElements);
 		return randomizedQueue[randomIndex];
 	}
 
 	// return an independent iterator over items in random order
-	public Iterator<Item> iterator() {	
-		randomObject.setSeed(System.currentTimeMillis());
+	public Iterator<Item> iterator() {
+		randomIndex = randomObject.nextInt(numberOfElements);
 		
 		Iterator<Item> it = new Iterator<Item>() {
-			int itCounter=0;
+			int itCounter = 0;
 			
 			@Override
 			public boolean hasNext() {
-				if (itCounter < numberOfElements) {
-					return true;
-				}
-				return false;
+				return itCounter < numberOfElements;
 			}
-
+			
 			@Override
 			public Item next() {
-				Random random = new Random();
-				random.setSeed(System.currentTimeMillis() + itCounter);
-				randomIndex = random.nextInt(numberOfElements);
+				if (randomIndex == numberOfElements-1) {
+					randomIndex=0;
+				}
+				else {
+					randomIndex++;
+				}
 				lastIndex = randomIndex;
 				itCounter++;
 				return randomizedQueue[randomIndex];
@@ -97,8 +122,7 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
 
 			@Override
 			public void remove() {
-				randomizedQueue[randomIndex] = randomizedQueue[numberOfElements];
-				numberOfElements--;
+				throw new UnsupportedOperationException();
 			}
 		};
 		return it;
@@ -107,19 +131,17 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
 	@Override
 	public String toString() {
 		StringBuilder stringBuilder = new StringBuilder();
-		for (int i=0; i<numberOfElements; i++)
+		for (int i = 0; i < numberOfElements; i++)
 			stringBuilder.append(randomizedQueue[i] + " ");
 		return stringBuilder.toString();
 	}
-	
+
 	// unit testing
 	public static void main(String[] args) {
 		TestRandomizedQueue test = new TestRandomizedQueue();
-			
 		test.testEnqueue();
 		test.testDequeue();
 		test.toString();
 		test.testIterator();
-		
 	}
 }
